2639945dfdbffe63f2e87e60fe704fb74497e340
Manna Soumya <sm323265@gmail.com>
2018-11-17 01:14:11 +0530

    Some major changes and added functionality of  Downloading Video files too
    Still downloading photos and videos in 'mp4' format but will refactor the code and add more functionalities to detect type of video in posts and download with that specific file format. 😄 👨‍💻

--- script.py
+++ script.py
@@ -1,22 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 import random
-print("This script can only download photos from Public Instagram Profiles")
-url=input("\nEnter Instagram Photo URL  (e.g. https://www.instagram.com/p/.../):- ")
+
+def download(url,option):    
+    if(option==1):
+        f = open((str(random.randint(1,50000)))+".jpg",'wb')
+    elif(option==2):
+        f = open((str(random.randint(1,50000)))+".mp4",'wb')
+    f.write(requests.get(url).content)
+    f.close()
+    print("Your Media is downloaded in the directory this script is running on..")
+    
+print()
+print("This script can only download photos and videos from \"Public Instagram Profiles\"")
+url=input("\nEnter Instagram Post URL  :- ")
 r=requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
-
-
 outertag=soup.find_all('meta')
 l=len(outertag)
+prop=[]
+for i in range(0,l):
+    prop.append(outertag[i].get('property'))
+
+vc = False
+for t in prop:
+    if (t == "og:video"):
+        vc=True
 
 for i in range(0,l):
     k=outertag[i].get('property')
-    if(k=="og:image"):
+    if (vc==True and k == "og:video"):
         content=outertag[i].get('content')
+        download(content,2)
         break
-
-print("Your Photo is downloaded in the directory this script is running on...!!")
-f = open((str(random.randint(1,50000)))+".jpg",'wb')
-f.write(requests.get(content).content)
-f.close()
+    if (vc==False and k == "og:image"):
+        content=outertag[i].get('content')
+        download(content,1)
+        break
